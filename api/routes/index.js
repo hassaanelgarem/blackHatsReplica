b03f95523fb081a60444324cff8d141d16e0fc5e
mirna elgazzar <mernashg@gmail.com>
2017-04-01 17:54:49 +0200

    deleted business.controller

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,10 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const userCtrl = require('../controllers/user.controller');
-//const businessCtrl = require('../controllers/business.controller');
 
 
-//router.route('/add').post(businessCtrl.add);
 router.route('/privacy').get(userCtrl.privacy);
 router.route('/terms').get(userCtrl.terms);
 router.route('/business/:businessId/addfavorite').get(userCtrl.addFavorite); 
