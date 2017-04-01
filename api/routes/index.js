b5eb08c50ff46f5872df3b96d71d26d6b8ef0759
mirna elgazzar <mernashg@gmail.com>
2017-04-01 17:29:08 +0200

    modification to function

--- api/routes/index.js
+++ api/routes/index.js
@@ -5,8 +5,8 @@ const userCtrl = require('../controllers/user.controller');
 
 
 //router.route('/add').post(businessCtrl.add);
-router.route('/privacy').get('userCtrl.privacy');
-router.route('/terms').get('userCtrl.terms');
-router.route('/business/:businessId/addfavorite').get('userCtrl.addFavorite'); 
+router.route('/privacy').get(userCtrl.privacy);
+router.route('/terms').get(userCtrl.terms);
+router.route('/business/:businessId/addfavorite').get(userCtrl.addFavorite); 
 
 module.exports = router;
