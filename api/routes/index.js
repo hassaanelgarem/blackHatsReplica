5d3c21eba634a38e050729db397c57ba78eb71ae
Aya Fayad <aya.fayad@student.guc.edu.eg>
2017-04-01 06:56:12 +0200

    extra method removed

--- api/routes/index.js
+++ api/routes/index.js
@@ -6,29 +6,3 @@ const reviewCtrl = require('../controllers/review.controller');
 router.route('/review').post('reviewCtrl.addReview');
 
 module.exports = router;
-
-.post(function(req, res) {
-        // Get values from POST request. These can be done through forms or REST calls. These rely on the "name" attributes for forms
-        
-        }, function (err, blob) {
-              if (err) {
-                  res.send("There was a problem adding the information to the database.");
-              } else {
-                  //Blob has been created
-                  console.log('POST creating new blob: ' + blob);
-                  res.format({
-                      //HTML response will set the location and redirect back to the home page. You could also create a 'success' page if that's your thing
-                    html: function(){
-                        // If it worked, set the header so the address bar doesn't still say /adduser
-                        res.location("blobs");
-                        // And forward to success page
-                        res.redirect("/blobs");
-                    },
-                    //JSON response will show the newly created blob
-                    json: function(){
-                        res.json(blob);
-                    }
-                });
-              }
-        })
-    });
