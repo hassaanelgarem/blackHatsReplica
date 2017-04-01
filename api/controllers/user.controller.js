64de66f45714a74161cc3ee683eba223c7c58b1f
Omar Doma <omar.elias.96@gmail.com>
2017-04-01 18:42:31 +0200

    initial commit

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,13 +1,23 @@
 const mongoose = require("mongoose");
 const Business = mongoose.model("Business");
 
+
+/*Search he Business model for businesses with name or tag 
+entered by the user, it gets all businesses with matching names
+and tags and returns them to the frontend
+Calling route: "/api/search" */
 module.exports.searchByNameOrTag = function (req, res) {
+    //Check for query string Ex: "/api/search?result=omar"
     if (req.query && req.query.result) {
         var nameOrTag = req.query.result;
+        
+        //Find businesses from the database
         Business.find({
                 $or: [{
                         name: {
+                            //Starts with or is the nameOrTag
                             $regex: "^" + nameOrTag,
+                            //Ignore whitespace characters and case insensitive
                             $options: "ix"
                         }
                     },
@@ -20,13 +30,16 @@ module.exports.searchByNameOrTag = function (req, res) {
                 ]
             },
             function (err, businesses) {
+                //If an error occured return it to the frontend
                 if (err) {
                     res.json(err);
                 } else {
+                    //return an array of businesses or an empty array
                     res.json(businesses);
                 }
             }
         );
     } else
+        //return an empty array
         res.json([]);
 };
