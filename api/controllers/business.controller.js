b4d718aacc61da85d04a28c9c3c8899b2561a7f4
Hassaan El-Garem <hassaanelgarem@gmail.com>
2017-04-01 12:52:39 +0200

    documentation and small changes

--- api/controllers/business.controller.js
+++ api/controllers/business.controller.js
@@ -13,8 +13,12 @@ module.exports.add = function(req, res){
 };
 */
 
+
+
+// Post function that increments the interactivity attribute of a certain business by 1
+// URI: api/business/interact/:id
 module.exports.updateInteractivity = function(req, res){
-  Business.findById(req.body.id, function(err, business){
+  Business.findById(req.params.id, function(err, business){
     business.interactivity = business.interactivity + 1;
     business.save(function(err){
       if(err) res.json({success: false, msg: 'Updating business interactivity failed'});
@@ -23,6 +27,9 @@ module.exports.updateInteractivity = function(req, res){
   });
 };
 
+
+// Get function that returns the three most popular businesses based on their interactivity
+// URI: api/business/mostPopular
 module.exports.getMostPopular = function(req, res){
   const query = Business.find().sort({interactivity: -1}).limit(3);
   query.exec(function(err, businesses){
--- api/routes/index.js
+++ api/routes/index.js
@@ -3,8 +3,11 @@ const router = express.Router();
 const businessCtrl = require('../controllers/business.controller');
 
 
-router.route('/add').post(businessCtrl.add);
-router.route('/business/interact').post(businessCtrl.updateInteractivity);
+//router.route('/add').post(businessCtrl.add);
+
+
+
+router.route('/business/interact/:id').post(businessCtrl.updateInteractivity);
 router.route('/business/mostPopular').get(businessCtrl.getMostPopular);
 
 module.exports = router;
