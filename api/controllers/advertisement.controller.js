f310b474328b2d9d09a954add84f94d820559c4c
marwanamr3 <marwan.abdelgawad@student.guc.edu.eg>
2017-04-02 15:23:34 +0200

    two models and two functions added

--- api/controllers/advertisement.controller.js
+++ api/controllers/advertisement.controller.js
@@ -5,16 +5,28 @@ const AdvSlot = mongoose.model("AdvSlot");
 
 /* Get function that retrieves the AdvSlots that appear on the homepage from the database
    URI: api/ads */
-module.exports.getAdvSlots = function(req, res){
+module.exports.getAdvSlots = function (req, res) {
   //finds all advSlots that can appear on homepage
-  AdvSlot.find({},{name: 1, price: 1, length: 1, width: 1},function(err, advSlot){
+  AdvSlot.find({}, {
+    name: 1,
+    price: 1,
+    length: 1,
+    width: 1
+  }, function (err, advSlot) {
     // If there is an error return it in response
     if (err) {
-      return res.json({success: false, msg: "Error retrieving advSlots", error: err});
-    }
-    else{
+      return res.json({
+        success: false,
+        msg: "Error retrieving advSlots",
+        error: err
+      });
+    } else {
       //returns an array of AdvSlots
-      res.json({success: true, msg: 'Successful retrieval', advSlot});
+      res.json({
+        success: true,
+        msg: 'Successful retrieval',
+        advSlot
+      });
     }
   })
 };
@@ -22,30 +34,42 @@ module.exports.getAdvSlots = function(req, res){
 
 /* Post function that handles booking an advertisement slot
    URI: /api/business/bookAdvSlot */
-module.exports.bookAdvSlot = function(req,res){
-    // Create new AdvBooking object using parameters from request
-    const newAdvBooking = new AdvBooking({
-        business: req.body.business,
-        advSlot: req.body.advSlot,
-        image: req.body.image,
-        startTime: req.body.startTime,
-        endTime: req.body.endTime
+module.exports.bookAdvSlot = function (req, res) {
+  // Create new AdvBooking object using parameters from request
+  const newAdvBooking = new AdvBooking({
+    business: req.body.business,
+    advSlot: req.body.advSlot,
+    image: req.body.image,
+    startTime: req.body.startTime,
+    endTime: req.body.endTime
+  });
+  // Save new booking in database
+  AdvBooking.save(function (err, booking) {
+    // If there is an error return it in response
+    if (err) return res.json({
+      success: false,
+      msg: 'Error adding the booking',
+      error: err
     });
-     // Save new booking in database
-     AdvBooking.save(function(err, booking){
-     // If there is an error return it in response
-     if (err) return res.json({success: false, msg: 'Error adding the booking', error: err});
-      // Find advSlot by its id and insert the booking id in its advSchedule array
-      AdvSlot.findByIdAndUpdate(
-        booking.advSlot,
-        {$push:{"advSchedule": booking._id}},
-        {safe: true, upsert: true, new: true},
-        function(err, advSlot){
-          // If there is an error return it in response
-          if(err) res.json({success: false, msg: "Error updating Adv Slot", error: err});
+    // Find advSlot by its id and insert the booking id in its advSchedule array
+    AdvSlot.findByIdAndUpdate(
+      booking.advSlot, {
+        $push: {
+          "advSchedule": booking._id
         }
-      );
-     })
-    };
-
-
+      }, {
+        safe: true,
+        upsert: true,
+        new: true
+      },
+      function (err, advSlot) {
+        // If there is an error return it in response
+        if (err) res.json({
+          success: false,
+          msg: "Error updating Adv Slot",
+          error: err
+        });
+      }
+    );
+  })
+};
