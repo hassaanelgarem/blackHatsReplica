7734e342adc747e9aefccb74d96985bf9bbe960a
Aya Fayad <aya.fayad@student.guc.edu.eg>
2017-04-02 09:54:27 +0200

    Initial testing done

---
+++ api/controllers/review.controller.js
@@ -0,0 +1,51 @@
+const mongoose = require('mongoose');
+const User = mongoose.model('User');
+const Business = mongoose.model('Business');
+const Review = mongoose.model('Review');
+
+
+/* Get function that retrieves the reviews made by a user from the database
+and displays them
+URI: api/review/user/:userID */
+module.exports.getUserReviews = function(req, res){
+  //Finds all reviews made by a user according to the User ID
+  Review.find({"user" : req.params.userID}, function(err, reviews){
+    //If an error occurred, return an error
+    if(err){
+      res.status(500).send(err);
+    }
+    else{
+      //returns an array of reviews
+      res.json({success: true, msg: 'successful retrieval', reviews});
+    }
+  });
+}
+
+
+/*
+Post function that handles editing an existing review
+It retrieves the review from the database, updates it
+and saves it back in the database
+URI: api/review/edit/:reviewID
+parameters: {
+  newComment: "The new comment as specified by the user"
+  newRating: "The new rating as specified by the user"
+}
+*/
+module.exports.editReview = function(req, res){
+  //gets values of variables that user wants to edit
+  const newComment = req.body.comment;
+  const newRating = req.body.rating;
+
+  //Finds the review by the ID specified in the URI and updates the comment and the rating
+  Review.findByIdAndUpdate(
+    req.params.reviewID,
+    {"comment" : newComment, "rating" : newRating},
+    {safe: true, new: true},
+      function(err, editedReview){
+      //If error occurred return it in response
+      if (err) return res.json({success: false, msg: 'Failed to edit review'});
+      //If no error occurs, response with success = true
+      res.json({success: true, msg: 'Review successfully edited'});
+    });
+};
--- api/routes/index.js
+++ api/routes/index.js
@@ -2,9 +2,13 @@ const express = require('express');
 const router = express.Router();
 const userCtrl = require('../controllers/user.controller');
 const businessCtrl = require('../controllers/business.controller');
+const reviewCtrl = require('../controllers/review.controller');
 
 
 router.route('/search').get(userCtrl.searchByNameOrTag);
 router.route('/business/interact/:id').post(businessCtrl.updateInteractivity);
 router.route('/business/mostPopular').get(businessCtrl.getMostPopular);
+router.route('/review/user/:userID').get(reviewCtrl.getUserReviews);
+router.route('/review/edit/:reviewID').post(reviewCtrl.editReview);
+
 module.exports = router;
