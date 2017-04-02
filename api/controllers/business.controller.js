6f7b064250702243dc0a4e869945d634de385bdb
Omar Doma <omar.elias.96@gmail.com>
2017-04-02 17:55:33 +0200

    Upload adds only the image name not the whole path to db and fixed deletePhoto

--- api/controllers/business.controller.js
+++ api/controllers/business.controller.js
@@ -6,18 +6,6 @@ const fs = require('fs');
 const path = require("path");
 
 
-// for testing
-/*
-module.exports.add = function(req, res){
-    const newBusiness = new Business({name: "test4", email: "test4", password: "test4", description: "test4"});
-    newBusiness.save(function (err, business) {
-      if (err) return res.json({success: false, msg: 'adding failed'});
-      res.json({success: true, msg: 'added'});
-    });
-};
-*/
-
-
 /* Multer configuration to upload a single file from an
 html input with name "myfile" to public/uploads/businessPhotos folder*/
 const uploadPhotos = multer({
@@ -35,73 +23,77 @@ Calling route: '/business/:businessId/addPhoto'
 module.exports.addPhoto = function (req, res) {
   //Check if business logged in
   if (req.user) {
-    //upload the image
-    uploadPhotos(req, res, function (err) {
-      //if an error occurred, return the error
-      if (err) {
-        return res.json(err);
-      }
-      /*if multer found a file selected
-      and image was uploaded successfully, 
-      multer will save the image in req.file*/
-      if (req.file) {
-        //get the image format
-        var string = req.file.originalname.substring(req.file.originalname.length - 3, req.file.originalname.length);
+  //upload the image
+  uploadPhotos(req, res, function (err) {
+    //if an error occurred, return the error
+    if (err) {
+      return res.json(err);
+    }
+    /*if multer found a file selected
+    and image was uploaded successfully, 
+    multer will save the image in req.file*/
+    if (req.file) {
+      //get the image format
+      var string = req.file.originalname.substring(req.file.originalname.length - 3, req.file.originalname.length);
 
-        //if it was jpeg add a "j" to the returned "peg"
-        if (string === "peg")
-          string = "j" + string;
+      //if it was jpeg add a "j" to the returned "peg"
+      if (string === "peg")
+        string = "j" + string;
 
-        //check if it is not a valid image format
-        if (!(string === "png" || string === "jpg" || string === "jpeg")) {
-          //delete the uploaded file
-          fs.unlink(req.file.path);
+      //check if it is not a valid image format
+      if (!(string === "png" || string === "jpg" || string === "jpeg")) {
+        //delete the uploaded file
+        fs.unlink(req.file.path);
 
-          //return the error message to frontend
-          return res.json({
-            error: "File format is not supported!"
-          });
-        }
-        //copy and rename the image to the following format and location
-        var newPath = path.join(__dirname, "../", "../public/uploads/businessPhotos/img" + Date.now() + "." + string);
-        fs.renameSync(req.file.path, newPath, function (err) {
-          if (err) throw err;
-
-          //delete the image with the old name
-          fs.unlink(req.file.path);
+        //return the error message to frontend
+        return res.json({
+          error: "File format is not supported!"
         });
+      }
+      //copy and rename the image to the following format and location
+      var newPath = path.join(__dirname, "../", "../public/uploads/businessPhotos/img" + Date.now() + "." + string);
+      fs.renameSync(req.file.path, newPath, function (err) {
+        if (err) throw err;
 
-        //add the image file path to the photos array of the Business model
-        Business.update({
+        //delete the image with the old name
+        fs.unlink(req.file.path);
+      });
+
+      //get the name part only from the uploaded image
+      var nameLength = ("img" + Date.now() + string).length + 1;
+      newPath = newPath.substring(newPath.length - nameLength);
+
+      //add the image file name to the photos array of the Business model
+      Business.update({
           "_id": req.params.businessId
         }, {
-            $push: {
-              "photos": newPath
-            }
-          },
-          function (err, result) {
-            //couldn't add to array, return the error
-            if (err) {
-              res.json(err);
-            } else {
-              //if found business
-              if (result) {
-                //return the file path to the frontend to show the image
-                res.json(newPath);
-              } else
-                res.json({
-                  error: "No business found"
-                });
-            }
-          });
-      }
-      //multer did not find a file selected to upload
-      else {
-        res.json({
-          error: "Choose a valid file"
+          $push: {
+            "photos": newPath
+          }
+        },
+        function (err, result) {
+          //couldn't add to array, return the error
+          if (err) {
+            res.json(err);
+          } else {
+            //if updating is ok
+            if (result) {
+              //return the file path to the frontend to show the image
+              res.json(newPath);
+            } else
+              res.json({
+                error: "No business found"
+              });
+          }
         });
-      }
-    });
+    }
+    //multer did not find a file selected to upload
+    else {
+      res.json({
+        error: "Choose a valid file"
+      });
+    }
+  });
   }
   //user is not logged in
   else {
@@ -119,29 +111,34 @@ Calling route: '/business/:businessId/deletePhoto/:photoPath'
 */
 module.exports.deletePhoto = function (req, res) {
   var imagePath = req.params.photoPath;
-  var businessId = req.params.businessID;
-  Business.findOneAndUpdate({
+  var businessId = req.params.businessId;
+  Business.update({
     "_id": businessId
   }, {
-      $pull: {
-        "photos": imagePath
-      }
-    }, function (err, data) {
+    $pull: {
+      "photos": imagePath
+    }
+  }, function (err, data) {
+    if (err) {
+      res.json({
+        success: false,
+        msg: 'deleting photo failed'
+      });
+    } else {
 
-      if (err) {
-        res.json({
-          success: false,
-          msg: 'deleting photo failed'
-        });
-      } else {
-        res.json({
-          success: true,
-          msg: 'photo deleted successfully'
-        });
-      }
+      //add directory path to image name
+      imagePath = path.join(__dirname, "../", "../public/uploads/businessPhotos/", req.params.photoPath);
 
-
-    });
+      //delete the photo from filesystem
+      fs.unlink(imagePath, function (err) {
+        //don't care if file doesn't exist
+      });
+      res.json({
+        success: true,
+        msg: 'photo deleted successfully'
+      });
+    }
+  });
 };
 
 
