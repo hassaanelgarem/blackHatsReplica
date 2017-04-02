b34ba93904188a6513d9d520f8642dc891071f29
sherif hussien <sherifmhmd96@gmail.com>
2017-04-02 20:19:28 +0200

    fixed coding standards

--- api/controllers/profile.controller.js
+++ api/controllers/profile.controller.js
@@ -1,29 +1,29 @@
-const mongoose=require('mongoose');
-const User=mongoose.model('User');
-const Review=mongoose.model('Review');
+const mongoose = require('mongoose');
+const User = mongoose.model('User');
+const Review = mongoose.model('Review');
 
 
-/*Get User info from the User model with _id
+/*Get function, to Get User info from the User model with _id
 equal to the paraams.userID and return it
 Calling route: '/api/profile/:userId'
 */
-module.exports.getOneUser =function(req,res){
-  var userId=req.params.userId;
+module.exports.getOneUser = function (req, res) {
+  var userId = req.params.userId;
 
   // finds user with the userId from the User model
-  User
-    .findById(userId)
-    .exec(function(err,doc){
-      //if an error to find the user,I return the error message
-      if(err){
-        res.status(500).json(err);
+  User.findById(userId).exec(function (err, doc) {
+    //if an error to find the user,I return the error message
+    if (err) {
+      res.status(500).json(err);
       //if no user with that userId was found,I return an error message
-      }else if(!doc){
-        res.status(404).json({"message":"userId not found "+userId});
-      }
-      //when the user is found successfully,I return the user
-      else{
-        res.status(200).json(doc);
-      }
-    });
+    } else if (!doc) {
+      res.status(404).json({
+        "message": "userId not found " + userId
+      });
+    }
+    //when the user is found successfully,I return the user
+    else {
+      res.status(200).json(doc);
+    }
+  });
 };
--- api/routes/index.js
+++ api/routes/index.js
@@ -2,13 +2,7 @@ const express = require('express');
 const router = express.Router();
 const userCtrl = require('../controllers/user.controller');
 const businessCtrl = require('../controllers/business.controller');
-const profileCtrl=require('../controllers/profile.controller.js');
-
-<<<<<<< HEAD
-router.route('/profile/:userId').get(profileCtrl.getOneUser);
-
-=======
->>>>>>> 4b8ff048a75293d99ea97d86c0deb7cbc9a1ecd1
+const profileCtrl = require('../controllers/profile.controller.js');
 
 router.route('/search').get(userCtrl.searchByNameOrTag);
 router.route('/business/interact/:id').post(businessCtrl.updateInteractivity);
