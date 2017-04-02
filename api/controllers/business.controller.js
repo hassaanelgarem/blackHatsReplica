ee052cbb6a2481e9257b81421d72f05004da1fa7
Omar Doma <omar.elias.96@gmail.com>
2017-04-02 04:43:50 +0200

    Handled an exception in upload

--- api/controllers/business.controller.js
+++ api/controllers/business.controller.js
@@ -21,7 +21,7 @@ module.exports.add = function(req, res){
 /* Multer configuration to upload a single file from an
 html input with name "myfile" to public/uploads/businessPhotos folder*/
 const uploadPhotos = multer({
-    dest: path.join(__dirname, '../', '../public/uploads/businessPhotos')
+  dest: path.join(__dirname, '../', '../public/uploads/businessPhotos')
 }).single('myfile');
 
 
@@ -33,71 +33,82 @@ filepath to the frontend to show the image.
 Calling route: '/business/:businessId/addPhoto'
 */
 module.exports.addPhoto = function (req, res) {
-	//Check if business logged in
-	if (req.user) {
-		//upload the image
-		uploadPhotos(req, res, function (err) {
-			//if an error occurred, return the error
-			if (err) {
-				return res.json(err);
-			}
-			/*if multer found a file selected
-			and image was uploaded successfully, 
-			multer will save the image in req.file*/
-			if (req.file) {
-				//get the image format
-				var string = req.file.originalname.substring(req.file.originalname.length - 3, req.file.originalname.length);
-				
-				//if it was jpeg add a "j" to the returned "peg"
-				if (string === "peg")
-					string = "j" + string;
+  //Check if business logged in
+  if (req.user) {
+    //upload the image
+    uploadPhotos(req, res, function (err) {
+      //if an error occurred, return the error
+      if (err) {
+        return res.json(err);
+      }
+      /*if multer found a file selected
+      and image was uploaded successfully, 
+      multer will save the image in req.file*/
+      if (req.file) {
+        //get the image format
+        var string = req.file.originalname.substring(req.file.originalname.length - 3, req.file.originalname.length);
 
-				//check if it is not a valid image format
-				if (!(string === "png" || string === "jpg" || string === "jpeg")) {
-					//delete the uploaded file
-					fs.unlink(req.file.path);
+        //if it was jpeg add a "j" to the returned "peg"
+        if (string === "peg")
+          string = "j" + string;
 
-					//return the error message to frontend
-					return res.json({
-						error: "File format is not supported!"
-					});
-				}
-				//copy and rename the image to the following format and location
-				var newPath = path.join(__dirname, "../", "../public/uploads/businessPhotos/img" + Date.now() + "." + string);
-				fs.renameSync(req.file.path, newPath, function (err) {
-					if (err) throw err;
+        //check if it is not a valid image format
+        if (!(string === "png" || string === "jpg" || string === "jpeg")) {
+          //delete the uploaded file
+          fs.unlink(req.file.path);
 
-					//delete the image with the old name
-					fs.unlink(req.file.path);
-				});
-                
-                //add the image file path to the photos array of the Business model
-                Business.update(
-                 {"_id": req.params.businessId },
-                 {$push: {"photos": newPath }},
-                 function(err, result) {
-                    //couldn't add to array, return the error
-                    if (err) {
-                        res.json(err);
-                     }
-                    else{
-                        //return the file path to the frontend to show the image
-			            res.json(newPath);
-                    }
+          //return the error message to frontend
+          return res.json({
+            error: "File format is not supported!"
+          });
+        }
+        //copy and rename the image to the following format and location
+        var newPath = path.join(__dirname, "../", "../public/uploads/businessPhotos/img" + Date.now() + "." + string);
+        fs.renameSync(req.file.path, newPath, function (err) {
+          if (err) throw err;
+
+          //delete the image with the old name
+          fs.unlink(req.file.path);
+        });
+
+        //add the image file path to the photos array of the Business model
+        Business.update({
+            "_id": req.params.businessId
+          }, {
+            $push: {
+              "photos": newPath
+            }
+          },
+          function (err, result) {
+            //couldn't add to array, return the error
+            if (err) {
+              res.json(err);
+            } else {
+              //if found business
+              if (result) {
+                //return the file path to the frontend to show the image
+                res.json(newPath);
+              } else
+                res.json({
+                  error: "No business found"
                 });
-			} 
-			//multer did not find a file selected to upload
-			else {
-				res.json({
-					error: "Choose a valid file"
-				});
-			}
-		});
-	} 
-	//user is not logged in
-	else {
-		res.json({error : "login"});
-	}
+            }
+          });
+      }
+      //multer did not find a file selected to upload
+      else {
+        res.json({
+          error: "Choose a valid file"
+        });
+      }
+    });
+  }
+  //user is not logged in
+  else {
+    res.json({
+      error: "login"
+    });
+  }
 };
 
 
@@ -106,33 +117,50 @@ delete function that deletes photo from business'
 photos array, and returns success message or error message.
 Calling route: '/business/:businessId/deletePhoto/:photoPath'
 */
-module.exports.deletePhoto = function(req, res){
-    var imagePath = req.params.photoPath;
-    var businessId = req.params.businessID;
-    Business.findOneAndUpdate({"_id": businessId}, {$pull: {"photos": imagePath}}, function(err, data){
-        
-        if(err){
-            res.json({success: false, msg: 'deleting photo failed'});
-        } 
-        else{
-            res.json({success: true, msg: 'photo deleted successfully'});
-        }
-        
+module.exports.deletePhoto = function (req, res) {
+  var imagePath = req.params.photoPath;
+  var businessId = req.params.businessID;
+  Business.findOneAndUpdate({
+    "_id": businessId
+  }, {
+    $pull: {
+      "photos": imagePath
+    }
+  }, function (err, data) {
 
+    if (err) {
+      res.json({
+        success: false,
+        msg: 'deleting photo failed'
       });
+    } else {
+      res.json({
+        success: true,
+        msg: 'photo deleted successfully'
+      });
+    }
+
+
+  });
 };
 
 
 /* 
 Post function that increments the interactivity attribute of a certain business by 1
 Calling route: api/business/interact/:id
-*/ 
-module.exports.updateInteractivity = function(req, res){
-  Business.findById(req.params.id, function(err, business){
+*/
+module.exports.updateInteractivity = function (req, res) {
+  Business.findById(req.params.id, function (err, business) {
     business.interactivity = business.interactivity + 1;
-    business.save(function(err){
-      if(err) res.json({success: false, msg: 'Updating business interactivity failed'});
-      res.json({success: true, msg: 'Business interactivity incremented'});
+    business.save(function (err) {
+      if (err) res.json({
+        success: false,
+        msg: 'Updating business interactivity failed'
+      });
+      res.json({
+        success: true,
+        msg: 'Business interactivity incremented'
+      });
     })
   });
 };
@@ -141,12 +169,20 @@ module.exports.updateInteractivity = function(req, res){
 /* 
 Get function that returns the three most popular businesses based on their interactivity
 Calling route: api/business/mostPopular
-*/ 
-module.exports.getMostPopular = function(req, res){
-  const query = Business.find().sort({interactivity: -1}).limit(3);
-  query.exec(function(err, businesses){
-      if(err) res.json({success: false, msg: 'Failed to retrieve most popular businesses'});
-      res.json({success: true, msg: 'Got most popular businesses successfully', businesses: businesses});
+*/
+module.exports.getMostPopular = function (req, res) {
+  const query = Business.find().sort({
+    interactivity: -1
+  }).limit(3);
+  query.exec(function (err, businesses) {
+    if (err) res.json({
+      success: false,
+      msg: 'Failed to retrieve most popular businesses'
+    });
+    res.json({
+      success: true,
+      msg: 'Got most popular businesses successfully',
+      businesses: businesses
+    });
   });
 };
-
