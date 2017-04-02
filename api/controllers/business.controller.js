51003ba993d260b4574247b5ad4e8068235f3e7e
RodainaMohamed <{rodinamohamed04@gmail.com}>
2017-04-02 22:38:48 +0200

    modifing code

--- api/controllers/business.controller.js
+++ api/controllers/business.controller.js
@@ -15,46 +15,6 @@ module.exports.add = function(req, res){
 
 
 
-// Post function that increments the interactivity attribute of a certain business by 1
-// URI: api/business/interact/:id
-module.exports.updateInteractivity = function (req, res) {
-	Business.findById(req.params.id, function (err, business) {
-		business.interactivity = business.interactivity + 1;
-		business.save(function (err) {
-			if (err) res.json({
-				success: false,
-				msg: 'Updating business interactivity failed'
-			});
-			res.json({
-				success: true,
-				msg: 'Business interactivity incremented'
-			});
-		})
-	});
-};
-
-
-// Get function that returns the three most popular businesses based on their interactivity
-// URI: api/business/mostPopular
-module.exports.getMostPopular = function (req, res) {
-	const query = Business.find().sort({
-		interactivity: -1
-	}).limit(3);
-	query.exec(function (err, businesses) {
-		if (err) res.json({
-			success: false,
-			msg: 'Failed to retrieve most popular businesses'
-		});
-		res.json({
-			success: true,
-			msg: 'Got most popular businesses successfully',
-			businesses: businesses
-		});
-	});
-};
-
-
-
 /*save the choosen Category by the business in the database .
   business can choose only 1 Category
   Calling route: '/api/business/addCategory' */
