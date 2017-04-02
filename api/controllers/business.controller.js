edbae328e6ec12c7e1c233c445dd10429e35db5a
Omar Doma <omar.elias.96@gmail.com>
2017-04-02 19:18:29 +0200

    fixed saving info

--- api/controllers/business.controller.js
+++ api/controllers/business.controller.js
@@ -2,33 +2,6 @@ const mongoose = require("mongoose");
 const Business = mongoose.model('Business');
 
 
-// for testing by postman
-
-module.exports.add = function (req, res) {
-  Business.create({
-      name: req.body.name,
-      email: req.body.email,
-      password: req.body.password,
-      description: req.body.description
-    },
-    function (err, hotel) {
-      if (err) {
-        console.log("Error creating Business");
-        res
-          .status(400)
-          .json(err);
-      } else {
-        console.log("Business created!", hotel);
-        res
-          .status(201)
-          .json(hotel);
-      }
-    });
-};
-
-
-
-
 // Post function that increments the interactivity attribute of a certain business by 1
 // URI: api/business/interact/:id
 module.exports.updateInteractivity = function (req, res) {
@@ -68,111 +41,93 @@ module.exports.getMostPopular = function (req, res) {
 };
 
 
-/*redirects business to display "Edit View" 
-and pass business object to the frontend.
+/* Get function that gets the current data of the business
+and pass business object to the frontend to display it in edit view.
 Calling route: '/api/editBusiness/:businessId' */
-module.exports.displayEditView = function (req, res) {
+module.exports.getCurrentInfo = function (req, res) {
   //check if logged in 
-  // if (req.user) {
-  Business.findOne({
-    _id: req.params.businessId
-  }, function (err, business) {
-    //if error occured 
-    if (err) {
-      res.json(err);
+  if (req.user) {
+    Business.findOne({
+      _id: req.params.businessId
+    }, function (err, business) {
+      //if error occured 
+      if (err) {
+        res.json(err);
+      } else {
+        // if business found 
+        if (business)
+          res.json(business);
 
-    } else {
-      // if business found 
-      if (business)
-        return res.json(business);
-
-      //business not found 
-      else
-        res.json({
-          error: "Business not found!"
-        });
-    }
-  });
+        //business not found 
+        else
+          res.json({
+            error: "Business not found!"
+          });
+      }
+    });
+  }
   //user is not logged in
-}
-/* else {
+  else {
     res.json({
       error: "login"
     });
-  };*/
-//};
-
-
-
-
-
-
-var _splitArray = function (input) {
-  var output;
-  if (input && input.lenght > 0) {
-    output = input.split(":");
-  } else {
-    console.log("invalid input");
-    output = [];
-  }
-  return output;
+  };
 }
 
 
-/* service to save the edited business info in the database 
+
+/* Post function to save the edited business info in the database 
 and returns updated object to frontend.
 Calling route: '/api/editBusiness/:businessId'  */
 module.exports.saveNewInfo = function (req, res) {
 
   //if logged in
-  //if (req.user) {
-  Business.findOne({
-    _id: req.params.businessId
-  }, function (err, business) {
-    //if an error occurred, return the error
-    if (err) {
-      res.json(err);
-    } else {
-      // if business found in database its basic info will be saved 
-      if (business) {
-        business.name = req.body.name;
-        business.email = req.body.email;
-        business.password = req.body.password;
-        business.phoneNumbers=req.body.phoneNumber1;
-        business.phoneNumbers=req.body.phoneNumber1;
-      // phoneNumbers : _splitArray(req.body.phoneNumbers);
+  if (req.user) {
+    Business.findOne({
+      _id: req.params.businessId
+    }, function (err, business) {
+      //if an error occurred, return the error
+      if (err) {
+        res.json(err);
+      } else {
+        // if business found in database its basic info will be saved 
+        if (business) {
+          business.name = req.body.name;
+          business.email = req.body.email;
+          business.password = req.body.password;
+          business.phoneNumbers = req.body.phoneNumber1;
+          business.phoneNumbers = req.body.phoneNumber2;
 
-        //Split workDays by "," to return an array of strings
-        business.workingDays.push(_splitArray(req.body.workingDays));
-        business.workingHours = {
-          from: req.body.from,
-          to: req.body.to
-        };
-        business.address = req.body.address;
-        business.description = req.body.description;
-        
-        business.save(function (err) {
-          if (err) {
-            res.json(err);
-          } else {
-            res.json(business);
-          }
-        });
-      }
-      //if business not found return this message to frontend 
-      else {
-        res.json({
-          error: "Business not found!"
-        });
-      }
-    }
-  });
-}
+          //Split workDays by "," to return an array of strings
+          business.workingDays = req.body.workingDays.split(",");
+          business.workingHours = {
+            from: req.body.from,
+            to: req.body.to
+          };
+          business.address = req.body.address;
+          business.description = req.body.description;
 
-//if business not logged in
-/*else {
+          business.save(function (err) {
+            if (err) {
+              res.json(err);
+            } else {
+              res.json(business);
+            }
+          });
+        }
+        //if business not found return this message to frontend 
+        else {
+          res.json({
+            error: "Business not found!"
+          });
+        }
+      }
+    });
+  }
+  //if business not logged in
+  else {
     res.json({
       error: "login"
     });
   }
-};*/
+};
