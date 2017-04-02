df49ebb9a7fe08899baa7635e616eaa1ada88815
Aya Fayad <aya.fayad@student.guc.edu.eg>
2017-04-02 18:41:51 +0200

    modified according to standards

--- api/controllers/review.controller.js
+++ api/controllers/review.controller.js
@@ -6,19 +6,19 @@ const Review = mongoose.model('Review');
 
 /* Get function that retrieves the reviews made by a user from the database
 and displays them
-URI: api/review/user/:userID */
-module.exports.getUserReviews = function(req, res){
+Calling route: api/review/user/:userID */
+module.exports.getUserReviews = function(req, res) {
   //Finds all reviews made by a user according to the User ID
-  Review.find({"user" : req.params.userID}, function(err, reviews){
+  Review.find({"user": req.params.userID}, function(err, reviews) {
     //If an error occurred, return an error
-    if(err){
+    if(err) {
       res.status(500).send(err);
     }
-    else{
+    else {
       //returns an array of reviews
       res.json({success: true, msg: 'successful retrieval', reviews});
     }
-  });
+  })
 }
 
 
@@ -26,13 +26,13 @@ module.exports.getUserReviews = function(req, res){
 Post function that handles editing an existing review
 It retrieves the review from the database, updates it
 and saves it back in the database
-URI: api/review/edit/:reviewID
+Calling route: api/review/edit/:reviewID
 parameters: {
   newComment: "The new comment as specified by the user"
   newRating: "The new rating as specified by the user"
 }
 */
-module.exports.editReview = function(req, res){
+module.exports.editReview = function(req, res) {
   //gets values of variables that user wants to edit
   const newComment = req.body.comment;
   const newRating = req.body.rating;
@@ -40,12 +40,12 @@ module.exports.editReview = function(req, res){
   //Finds the review by the ID specified in the URI and updates the comment and the rating
   Review.findByIdAndUpdate(
     req.params.reviewID,
-    {"comment" : newComment, "rating" : newRating},
+    {"comment": newComment, "rating": newRating},
     {safe: true, new: true},
-      function(err, editedReview){
+      function(err, editedReview) {
       //If error occurred return it in response
       if (err) return res.json({success: false, msg: 'Failed to edit review'});
       //If no error occurs, response with success = true
       res.json({success: true, msg: 'Review successfully edited'});
-    });
-};
+    })
+}
--- api/data/review.model.js
+++ api/data/review.model.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const reviewSchema = new mongoose.Schema({
+const reviewSchema = new mongoose.Schema( {
 	comment: {
 		type: String
 	},
