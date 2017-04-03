83bcd5c232e2635ce6f5995c5a5f42096c17e77c
Mohamed Yehia <mohamed@mohamedUb16.04>
2017-04-03 21:25:13 +0200

    Removed comments

--- api/controllers/activity.controller.js
+++ api/controllers/activity.controller.js
@@ -21,85 +21,85 @@ Calling route: '/activity/:activityId/addPhoto'
 */
 module.exports.addPhoto = function(req, res) {
     //Check if business is logged in
-    //if (req.user) {
-    //upload the image
-    uploadPhotos(req, res, function(err) {
-        //if an error occurred, return the error
-        if (err) {
-            return res.json(err);
-        }
-        /*if multer found a file selected
-        and image was uploaded successfully,
-        multer will save the image in req.file*/
-        if (req.file) {
-            //get the image format
-            var string = req.file.originalname.substring(req.file.originalname.length - 3, req.file.originalname.length);
+    if (req.user) {
+        //upload the image
+        uploadPhotos(req, res, function(err) {
+            //if an error occurred, return the error
+            if (err) {
+                return res.json(err);
+            }
+            /*if multer found a file selected
+            and image was uploaded successfully,
+            multer will save the image in req.file*/
+            if (req.file) {
+                //get the image format
+                var string = req.file.originalname.substring(req.file.originalname.length - 3, req.file.originalname.length);
 
-            //if it was jpeg add a "j" to the returned "peg"
-            if (string === "peg")
-                string = "j" + string;
+                //if it was jpeg add a "j" to the returned "peg"
+                if (string === "peg")
+                    string = "j" + string;
 
-            //check if it is not a valid image format
-            if (!(string === "png" || string === "jpg" || string === "jpeg")) {
-                //delete the uploaded file
-                fs.unlink(req.file.path);
+                //check if it is not a valid image format
+                if (!(string === "png" || string === "jpg" || string === "jpeg")) {
+                    //delete the uploaded file
+                    fs.unlink(req.file.path);
 
-                //return the error message to frontend
-                return res.json({
-                    error: "File format is not supported!"
+                    //return the error message to frontend
+                    return res.json({
+                        error: "File format is not supported!"
+                    });
+                }
+                //copy and rename the image to the following format and location
+                var newPath = path.join(__dirname, "../", "../public/uploads/activityPhotos/img" + Date.now() + "." + string);
+                fs.renameSync(req.file.path, newPath, function(err) {
+                    if (err) throw err;
+
+                    //delete the image with the old name
+                    fs.unlink(req.file.path);
+                });
+
+                //get the name part only from the uploaded image
+                var nameLength = ("img" + Date.now() + string).length + 1;
+                newPath = newPath.substring(newPath.length - nameLength);
+
+                //add the image file name to the photos array of the Business model
+                Activity.update({
+                        "_id": req.params.activityId
+                    }, {
+                        $push: {
+                            "photos": newPath
+                        }
+                    },
+                    function(err, result) {
+                        //couldn't add to array, return the error
+                        if (err) {
+                            res.json(err);
+                        } else {
+                            //if updating is ok
+                            if (result) {
+                                //return the file path to the frontend to show the image
+                                res.json(newPath);
+                            } else
+                                res.json({
+                                    error: "Activity not found"
+                                });
+                        }
+                    });
+            }
+            //multer did not find a file selected to upload
+            else {
+                res.json({
+                    error: "Choose a valid file"
                 });
             }
-            //copy and rename the image to the following format and location
-            var newPath = path.join(__dirname, "../", "../public/uploads/activityPhotos/img" + Date.now() + "." + string);
-            fs.renameSync(req.file.path, newPath, function(err) {
-                if (err) throw err;
-
-                //delete the image with the old name
-                fs.unlink(req.file.path);
-            });
-
-            //get the name part only from the uploaded image
-            var nameLength = ("img" + Date.now() + string).length + 1;
-            newPath = newPath.substring(newPath.length - nameLength);
-
-            //add the image file name to the photos array of the Business model
-            Activity.update({
-                    "_id": req.params.activityId
-                }, {
-                    $push: {
-                        "photos": newPath
-                    }
-                },
-                function(err, result) {
-                    //couldn't add to array, return the error
-                    if (err) {
-                        res.json(err);
-                    } else {
-                        //if updating is ok
-                        if (result) {
-                            //return the file path to the frontend to show the image
-                            res.json(newPath);
-                        } else
-                            res.json({
-                                error: "Activity not found"
-                            });
-                    }
-                });
-        }
-        //multer did not find a file selected to upload
-        else {
-            res.json({
-                error: "Choose a valid file"
-            });
-        }
-    });
-    //}
+        });
+    }
     //user is not logged in
-    //else {
-    //res.json({
-    //  error: "Please Login"
-    //});
-    //}
+    else {
+        res.json({
+            //  error: "Please Login"
+        });
+    }
 };
 
 
