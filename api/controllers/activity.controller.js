5fe09503a70301a0e036999bbc34c972c326d3a6
mirna elgazzar <mernashg@gmail.com>
2017-04-03 20:46:29 +0200

    fixed activity.controller and tested

--- api/controllers/activity.controller.js
+++ api/controllers/activity.controller.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const Activity = mongoose.model("Activity");
+const Business = mongoose.model("Business");
 
 
 /*
@@ -9,52 +10,52 @@ const Activity = mongoose.model("Activity");
   Calling route: '/api/activity/add'
 */
 module.exports.addActivity = function(req, res) {
-  //check if logged in
-  if(req.user){
-    // Create new Activity object using parameters from request
-    const newActivity = new Activity({
-        name: req.body.name,
-        price: req.body.price,
-        description: req.body.description,
-        bookingsPerSlot: req.body.bookingsPerSlot,
-        business: req.body.business
-    });
-    // Save new Activity in database
-    newActivity.save(function(err, activity) {
-        // If there is an error return it in response
-        if (err) return res.json({
-            success: false,
-            msg: 'Failed to Add Activity',
-            error: err
+    //check if logged in
+    if (req.user) {
+        // Create new Activity object using parameters from request
+        const newActivity = new Activity({
+            name: req.body.name,
+            price: req.body.price,
+            description: req.body.description,
+            bookingsPerSlot: req.body.bookingsPerSlot,
+            business: req.body.business
         });
+        // Save new Activity in database
+        newActivity.save(function(err, activity) {
+            // If there is an error return it in response
+            if (err) return res.json({
+                success: false,
+                msg: 'Failed to Add Activity',
+                error: err
+            });
 
-        // Find Business by their id and inserts the activity id in their activites array
-        Business.findByIdAndUpdate(
-            activity.business, {
-                $push: {
-                    "activities": activity._id
+            // Find Business by their id and inserts the activity id in their activites array
+            Business.findByIdAndUpdate(
+                activity.business, {
+                    $push: {
+                        "activities": activity._id
+                    }
+                }, {
+                    safe: true,
+                    upsert: true,
+                    new: true
+                },
+                function(err, business) {
+                    // If there is an error return it in response
+                    if (err) res.josn({
+                        success: false,
+                        error: err
+                    });
+                    // If no errors occur, respond with success = true
+                    res.json({
+                        success: true,
+                        msg: 'Activity Added'
+                    });
                 }
-            }, {
-                safe: true,
-                upsert: true,
-                new: true
-            },
-            function(err, business) {
-                // If there is an error return it in response
-                if (err) res.josn({
-                    success: false,
-                    error: err
-                });
-                // If no errors occur, respond with success = true
-                res.json({
-                    success: true,
-                    msg: 'Activity Added'
-                });
-            }
-        );
-    });
-  }   //User not logged in
-  else {
-    res.json({error : "Please Login"});
-  }
+            );
+        });
+    } //User not logged in
+    else {
+        res.json({ error: "Please Login" });
+    }
 };
