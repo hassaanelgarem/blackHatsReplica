d8fca952c4661ef08da14a1f859c2acb47e99580
Mohamed Yehia <mohamed@mohamedUb16.04>
2017-04-03 22:04:38 +0200

    correccted roue

--- api/routes/index.js
+++ api/routes/index.js
@@ -2,12 +2,12 @@ const express = require('express');
 const router = express.Router();
 const userCtrl = require('../controllers/user.controller');
 const businessCtrl = require('../controllers/business.controller');
-const actvityCtrl = require('../controllers/activity.controller');
+const activityCtrl = require('../controllers/activity.controller');
 
 
 router.route('/search').get(userCtrl.searchByNameOrTag);
 router.route('/business/interact/:id').post(businessCtrl.updateInteractivity);
 router.route('/business/mostPopular').get(businessCtrl.getMostPopular);
-router.route('/activity/add').post(actvityCtrl.addActivity);
+router.route('/activity/add').post(activityCtrl.addActivity);
 
 module.exports = router;
