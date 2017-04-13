2b66cc4ebac8e0eecb2025f9bfa29e488a5b16e3
Hassaan El-Garem <hassaanelgarem@gmail.com>
2017-04-13 22:46:23 +0200

    template assets and dummy component

--- assets/app/app.module.ts
+++ assets/app/app.module.ts
@@ -1,11 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { routing } from "./app.routing";
+
 
 import { AppComponent } from "./app.component";
+import { DummyComponent } from "./dummy/dummy.component";
+
+import { DummyService } from "./dummy/dummy.service";
 
 @NgModule({
-    declarations : [AppComponent],
-    imports: [BrowserModule],
+    declarations : [
+      AppComponent,
+      DummyComponent
+    ],
+    imports: [
+      BrowserModule,
+      FormsModule,
+      HttpModule,
+      routing
+    ],
+    providers: [DummyService],
     bootstrap : [AppComponent]
 })
 
