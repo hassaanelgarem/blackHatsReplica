17aa444d9eac7cdf321e47401549101c04d8cefc
Ramy El Zahaby <RamyyElZahaby@gmail.com>
2017-04-15 23:09:59 +0200

    Modified user register modal

--- assets/app/app.routing.ts
+++ assets/app/app.routing.ts
@@ -3,12 +3,14 @@ import { Routes, RouterModule } from "@angular/router";
 import { DummyComponent } from "./dummy/dummy.component";
 import { BusinessComponent } from "./business/business.component";
 import { HomepageComponent } from "./homepage/homepage.component";
+import { RegisterComponent } from "./user/register/register.component";
 
 
 const APP_ROUTES: Routes = [
     { path: 'homepage', component: HomepageComponent },
     { path: 'business', component: BusinessComponent },
-    { path: 'dummy', component: DummyComponent }
+    { path: 'dummy', component: DummyComponent },
+    { path: 'userRegister', component: RegisterComponent}
 ];
 
 export const routing = RouterModule.forRoot(APP_ROUTES);
