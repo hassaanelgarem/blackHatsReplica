de10420577800ccbfbd008d3d49fc89d540e748b
Akassed <akassed@gmail.com>
2017-04-17 00:00:14 +0200

    business tempalet finalized with bug

--- assets/app/businessPage/businessPage.component.ts
+++ assets/app/businessPage/businessPage.component.ts
@@ -11,14 +11,23 @@ export class BusinessPageComponent implements OnInit {
 
   name: String;
   address: String;
+  email: String;
   phoneNumbers: [String];
+  workingDays: [String];
+  workingHours: Object;
   totalRatings: Number;
+  description: String;
   rating: String;
   activities: [Object];
-  businessId: String = "58ee4577fa669f51b6abf3ae";
+  businessId: String = "58f28a95701573fd750a9b1c";
   path: String = "http://localhost:8080/api/";
   business: Object;
+  reviews: [Object];
   loadDone = false;
+  ownerLoggedIn = false;
+  userLoggedIn = false;
+  otherBusinessLoggedIn = false;
+  noLogIn = false;
 
   constructor(
     private businessPageService: BusinessPageService,
@@ -37,25 +46,38 @@ export class BusinessPageComponent implements OnInit {
                 this.address = info.data.address;
                 this.phoneNumbers = info.data.phoneNumbers;
                 this.totalRatings = info.data.totalRatings;
+                this.description = info.data.description;
+                this.workingDays = info.data.workingDays;
+                this.email = info.data.email;
+                this.workingHours = info.data.workingHours;
                 this.loadDone = true;
             }
         });
-    //
-        // this.businessPageService.getAverageRating(this.businessId).subscribe(info => {
-        //         if (info.err) {
-        //             console.error(info.msg);
-        //         }
-        //         else {
-        //             this.rating = info.data.toFixed(1);
-        //         }
-        //     });
-    //
+
+         this.businessPageService.getAverageRating(this.businessId).subscribe(info => {
+                 if (info.err) {
+                     console.error(info.msg);
+                 }
+                 else {
+                     this.rating = info.data.toFixed(1);
+                 }
+             });
+
         this.businessPageService.getActivities(this.businessId).subscribe(info => {
                 if (info.err) {
                     console.error(info.msg);
                 }
                 else {
                     this.activities = info.data;
+                }
+            });
+
+        this.businessPageService.getReviews(this.businessId).subscribe(info => {
+                if (info.err) {
+                    console.error(info.msg);
+                }
+                else {
+                    this.reviews = info.data;
                 }
             });
 
