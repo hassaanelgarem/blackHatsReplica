65155c0131ce3c192ebd2ef099011a489d5723b9
Hassaan El-Garem <hassaanelgarem@gmail.com>
2017-04-19 20:30:25 +0200

    nothing

--- assets/app/app.routing.ts
+++ assets/app/app.routing.ts
@@ -3,14 +3,12 @@ import { Routes, RouterModule } from "@angular/router";
 import { DummyComponent } from "./dummy/dummy.component";
 import { BusinessComponent } from "./business/business.component";
 import { HomepageComponent } from "./homepage/homepage.component";
-import { RegisterComponent } from "./user/register/register.component";
 
 
 const APP_ROUTES: Routes = [
     { path: 'homepage', component: HomepageComponent },
     { path: 'business', component: BusinessComponent },
-    { path: 'dummy', component: DummyComponent },
-    { path: 'userRegister', component: RegisterComponent}
+    { path: 'dummy', component: DummyComponent }
 ];
 
 export const routing = RouterModule.forRoot(APP_ROUTES);
